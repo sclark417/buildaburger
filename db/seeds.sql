47f88bf180781084983a38f87aaa3b9cf0fc4574
Stephen Clark <clark7382@hotmail.com>
2021-03-02 16:35:40 -0700

    making a change

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,4 +4,4 @@ INSERT INTO burgers (name) VALUES ('Classic Burger');
 INSERT INTO burgers (name) VALUES ('Turkey Burger');
 INSERT INTO burgers (name) VALUES ('Impossible Burger');
 INSERT INTO burgers (name) VALUES ('Gouda Burger');
-INSERT INTO burgers (name) Values ('Barbque Bacon')
+INSERT INTO burgers (name) Values ('Barbque Bacon');
